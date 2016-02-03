07dab2d212ee0a147e61eb482cf338f0f4fcd9aa
ramdanisource <ramdani.rizki19@gmail.com>
2016-02-03 11:09:20 +0700

    button back worked

--- app/src/main/java/com/example/ramdani/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/ramdani/myapplication/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.ramdani.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 
 /**
  * Created by ramdani on 2/3/2016.
@@ -19,5 +20,21 @@ public class SecondActivity extends AppCompatActivity{
 
         Log.d("posisi", "saya berada pada method onCreate pada activity kedua");
 
+    }
+
+    /**
+     * react to the user tapping the back/up icon in the action bar
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // this takes the user 'back', as if they pressed the left-facing triangle icon on the main android toolbar.
+                // if this doesn't work as desired, another possibility is to call `finish()` here.
+                this.onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
